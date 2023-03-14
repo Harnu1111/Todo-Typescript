b5cce2a06f8cdde745c3bd418c87dc601d512eaa
Anuoluwapo <jaguarjum@gmail.com>
2023-03-14 18:26:51 +0100

    complete

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ const list = document.getElementById("list");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault(false);
+
+ 
   const task = document.createElement("li");
   const item = document.createElement("item");
   const label = document.createElement("label");
   const check = document.createElement("input");
+  const deletebtn = document.createElement("button");
+
   check.type = "checkbox";
 
+  deletebtn.innerHTML = "Delete";
+  deletebtn.classList.add("deleteBtn");
+  
+
   item.innerHTML = input.value;
-  label.append(check, item)
+  label.append(check, item, deletebtn)
 task.append(label)
   list.append(task);
 
+  
+deletebtn.addEventListener("click", () => {
+  task.remove()
+})
 
   check.addEventListener("change", () => { 
     if(check.checked){
@@ -24,4 +36,8 @@ task.append(label)
    }
   })
  
+
+  input.value="";
 });
+
+ 
